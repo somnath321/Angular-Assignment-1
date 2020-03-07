31c5806e03455565019c5f2ab820761c97fcdd3a
somnath321 <somnath29197@gmail.com>
2020-03-07 14:10:49 +0530

    first commit

---
+++ employee/src/app/employee.model.ts
@@ -0,0 +1,13 @@
+export class Employee {
+  firstName:string;
+    lastName:string;
+    email:string;
+    contactNumber: number;
+    address: string;
+    userName: string;
+    password: string;
+    gender: string;
+    qualification: string;
+    experience: number;
+    codingLanguages:{}
+}
